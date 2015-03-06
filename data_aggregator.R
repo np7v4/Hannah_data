1521761f6a293d8c25b83337f7e39bfeca006ba6
unknown <np7v4@as-psych18.col.missouri.edu>
2015-03-06 14:51:03 -0600

    Fixed fixation column for Block 3 data.
    There's a bug in that steals from one fixation to another.
    I am sorry.  The changes will be fixed.

--- data_aggregator.R
+++ data_aggregator.R
@@ -1,4 +1,5 @@
 # Welcome! To data aggregator!
+#TO DO: handle bug on line 82 matches stuff it's not supposed to.
 
 # Read in the data file
 dat <- read.delim("./raw_data/Sub1.txt") 
@@ -68,6 +69,29 @@ for (i in nose_range) {
   dat$fixation[grep(i, dat$FaceFilename)] = "nose"
 } # not a problem now because it won't match 21 to 1
 # View(dat)
+sub2dat = dat[dat$ID == 2,]
+table(sub2dat$fixation, sub2dat$Block)
+
+#Fix block 3 fixation data too
+table(dat$FaceFilename[dat$Block==3]) #looks like range is 1-48
+temp = dat$FaceFilename[dat$Block==3]
+table(temp)
+
+# We can do a similar thing to fix the point of fixation
+forehead_range = c(1:8,25:32)
+eyes_range = c(9:16, 33:40) #BUG NEEDS TO BE FIXED
+nose_range = c(17:24, 41:48)
+
+for (i in forehead_range) {
+  dat$fixation[grepl(i, dat$FaceFilename) & dat$Block == 3] = "forehead"
+} # note that this will still match 1 with 11, 21, but we're gonna fix those later
+for (i in eyes_range) {
+  dat$fixation[grepl(i, dat$FaceFilename) & dat$Block ==3] = "eyes"
+}
+for (i in nose_range) {
+  dat$fixation[grepl(i, dat$FaceFilename) & dat$Block ==3] = "nose"
+} # not a problem now because it won't match 21 to 1
+
 sub2dat = dat[dat$ID == 2,]
 table(sub2dat$fixation, sub2dat$Block)
 
